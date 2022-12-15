286e915a04554a7d76584e4ada93a1565f22dc15
Ludovic <l.boda@qantis.co>
2022-12-15 11:44:20 +0100

    fix acount endpoint

--- src/Service/UpplerAuthenticationService.php
+++ src/Service/UpplerAuthenticationService.php
@@ -27,43 +27,7 @@ class UpplerAuthenticationService extends HttpClientProvider
         $session->clear();
 
         if (empty($account->getUpplerClientId()) || empty($account->getUpplerClientSecret())) {
-            //on demande l'obtention d'un token Admin Uppler
-            $this->getAdminToken();
-
-            if (null === $this->adminToken) {
-                return false;
-            }
-
-            // on sollicite des paramétres de connexion pour ce suer auprés d'Uppler
-            // si le username ou le password sont faux on le sait ici
-            $res = $this->request(
-                'POST',
-                $this->apiUrl . 'v1/user/access-token',
-                [
-                    "body" => [
-                        "username" => $account->getUpplerUsername(),
-                        "password" => $account->getUpplerPassword()
-                    ]
-                ],
-                true
-            );
-            // la réponse Uppler est OK on récupère les infos du tokenUser
-            if ($res && Response::HTTP_OK ===$res->getStatusCode()) {
-                $payload = json_decode($res->getContent());
-                $account->setUpplerClientId($payload->client_id);
-                $account->setUpplerClientSecret($payload->client_secret);
-                // grace aux paramétres de connexion on sollicite un accessToken pour ce user
-                $this->getUserToken($account);
-                //si l'accessToken a été récupéré il doit être en session,
-                // si c'est le cas on stocke  aussi les infos du tokenUser en session
-                // et les client_id/client_secret en bdd pour les réutiliser à chaque connexion
-                if ($session->has('access_token') && !empty($session->get('access_token'))) {
-                    $account->setLastConnexion(new \DateTime('now'));
-                    $this->em->persist($account);
-                    $this->em->flush();
-                    return true;
-                }
-            }
+            return false;
         } else {
             // grace aux paramétres de connexion déjà connus on sollicite un accessToken pour ce user
             $this->getUserToken($account);
