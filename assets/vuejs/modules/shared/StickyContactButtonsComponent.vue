91f6fdcccead5d1d399b274b8956eb44fda90ec3
dapaul-qantis <pe.digbeu@qantis.co>
2023-03-15 16:38:38 +0100

    main Ajout du numéro et du mail dans les deux petits boutons fixes à droite de la page

--- assets/vuejs/modules/shared/StickyContactButtonsComponent.vue
+++ assets/vuejs/modules/shared/StickyContactButtonsComponent.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="hidden sm:block">
-    <button class="sticky-right-button top-[280px]">
+    <RouterLink
+      :to="{ name: PageList.CONTACT_PAGE }"
+      class="sticky-right-button top-[280px] flex items-center justify-between pr-1"
+    >
       <MailIconComponent />
-    </button>
-    <button class="sticky-right-button top-[340px]">
+      <span>{{ MAIL_ANIMATION }}</span>
+    </RouterLink>
+    <button class="sticky-right-button top-[340px] flex items-center justify-between pr-1">
       <PhoneIconComponent />
+      <span>{{ PHONE_ANIMATION }}</span>
     </button>
   </div>
 </template>
@@ -12,11 +17,13 @@
 <script lang="ts" setup>
 import MailIconComponent from '@/vuejs/modules/shared/icon/MailIconComponent.vue'
 import PhoneIconComponent from '@/vuejs/modules/shared/icon/PhoneIconComponent.vue'
+import { MAIL_ANIMATION, PHONE_ANIMATION } from '../../services/const';
+import { PageList } from '@/vuejs/router';
 </script>
 
 <style lang="postcss">
 .sticky-right-button {
-  @apply fixed -right-[190px] z-50 h-[50px] w-[245px] rounded-l-full bg-gradient-to-r from-secondary via-gradient-1 to-gradient-2
+  @apply fixed -right-[205px] z-50 h-[50px] w-[260px] rounded-l-full bg-gradient-to-r from-secondary via-gradient-1 to-gradient-2
   pl-5 text-white;
   &:hover {
     @apply right-0;
