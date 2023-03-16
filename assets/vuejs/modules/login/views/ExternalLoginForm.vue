29fb4fede509bd353463c202af69454626ce5328
dapaul-qantis <113889695+dapaul-qantis@users.noreply.github.com>
2023-03-16 18:17:28 +0100

    MKP-10 Pop-up reconnaissance de SIRET (#58)

--- assets/vuejs/modules/login/views/ExternalLoginForm.vue
+++ assets/vuejs/modules/login/views/ExternalLoginForm.vue
@@ -1,100 +1,155 @@
 <template>
-  <div v-if="showAlert">
-    <AlertSharedComponent />
-  </div>
-  <template v-if="!userAccounts.length">
-    <form @submit.prevent="loginSubmit">
-      <h1 class="text-2xl font-bold text-primary xl:text-4xl">Bonjour</h1>
-      <div class="mt-3 text-lg text-gray-500">
-        Déjà adhérent ? Connectez-vous ici
-      </div>
-      <div class="relative mb-3 mt-3">
-        <input
-          v-model="username"
-          type="email"
-          class="input !pr-16"
-          placeholder="Adresse e-mail"
-          :disabled="isLoading"
-          required
-        />
-        <span
-          class="absolute inset-y-0 right-0 flex items-center pr-8 text-gray-500"
-        >
-          <MailIcon />
-        </span>
-      </div>
-      <div class="relative mb-3 mt-3">
-        <input
-          v-model="password"
-          :type="showPassword ? 'text' : 'password'"
-          placeholder="Mot de passe"
-          class="input !pr-16"
-          :disabled="isLoading"
-          required
-        />
-        <span
-          class="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-8 text-gray-500"
-          @click="toggleShowPassword"
-        >
-          <EyeSlashIcon v-if="showPassword" />
-          <EyeIcon v-else />
-        </span>
-      </div>
-      <div class="mb-3 mt-3 flex justify-between">
-        <a
-          href="/mot-de-passe-oublie"
-          class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >Mot de passe oublié ?</a
-        >
-        <a
-          href="/premiere-connexion"
-          class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >Première connexion</a
-        >
-      </div>
-      <div class="mt-3 items-center lg:mt-6 lg:flex">
-        <ButtonComponent
-          :is-loading="isLoading"
-          type="submit"
-          class="button-gradient min-w-[180px]"
-        >
-          <ArrowRightIcon />
-          Me connecter
-        </ButtonComponent>
 
-        <div class="text-gray-500 lg:ml-10 lg:flex">
-          <div class="pt-6 lg:pt-0">
-            Vous n'êtes pas encore adhérent ?<br />
-            <span class="text-secondary underline">
-              Et si on se rencontrait ?
-            </span>
-          </div>
-          <div class="py-6 xl:pt-0">
-            Ou appelez nous directement au <br />
-            <span class="text-secondary underline">{{ PHONE_ANIMATION }}</span>
-          </div>
+  <template v-if="!userAccounts.length">
+    <div class="flex flex-col md:flex-row justify-between md:space-x-8">
+      <div class="w-full md:w-1/2 flex">
+        <div v-if="showAlert">
+          <AlertSharedComponent />
         </div>
+        <form
+          class="w-full"
+          @submit.prevent="loginSubmit"
+        >
+          <h1 class="text-2xl font-bold text-primary xl:text-4xl">Bonjour</h1>
+          <div class="mt-3 text-lg text-gray-500">
+            Déjà adhérent ? Connectez-vous ici
+          </div>
+          <div class="lg:w-5/6">
+            <div class="relative mb-3 mt-3">
+              <input
+                v-model="username"
+                type="email"
+                class="input !pr-16"
+                placeholder="Adresse e-mail"
+                :disabled="isLoading"
+                required
+              />
+              <span
+                class="absolute inset-y-0 right-0 flex items-center pr-8 text-gray-500"
+              >
+              <MailIcon />
+            </span>
+            </div>
+            <div class="relative mb-3 mt-3">
+              <input
+                v-model="password"
+                :type="showPassword ? 'text' : 'password'"
+                placeholder="Mot de passe"
+                class="input !pr-16"
+                :disabled="isLoading"
+                required
+              />
+              <span
+                class="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-8 text-gray-500"
+                @click="toggleShowPassword"
+              >
+                <EyeSlashIcon v-if="showPassword" />
+                <EyeIcon v-else />
+              </span>
+            </div>
+            <div class="mb-3 mt-3 flex justify-between">
+              <a
+                href="/mot-de-passe-oublie"
+                class="ml-2 text-sm font-medium text-gray-500 dark:text-gray-300"
+              >Mot de passe oublié ?</a
+              >
+              <a
+                href="/premiere-connexion"
+                class="ml-2 text-sm font-medium text-gray-500 dark:text-gray-300"
+              >Première connexion</a
+              >
+            </div>
+            <div class="mt-3 lg:items-center flex flex-col lg:mt-6 lg:grid lg:grid-cols-3 lg:justify-items-stretch w-full mb-6 space-y-5 lg:space-y-0 ">
+              <div class="w-full lg:justify-self-start">
+                <ButtonComponent
+                  :is-loading="isLoading"
+                  type="submit"
+                  class="button-gradient min-w-[180px] w-full lg:w-auto"
+                >
+                  <ArrowRightIcon />
+                  Me connecter
+                </ButtonComponent>
+              </div>
+              <div class="text-gray-500 lg:justify-self-center">
+                Vous n'êtes pas encore adhérent ?<br />
+                <span class="text-secondary underline">
+                    Et si on se rencontrait ?
+                  </span>
+              </div>
+
+              <div class="flex flex-col text-gray-500 lg:justify-self-end">
+                <span class="lg:text-right">Ou appelez nous directement au </span>
+                <span class="text-secondary underline lg:text-right">{{ PHONE_ANIMATION }}</span>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
+      <div class="sm:w-1/2 flex">
+        <AchetonsEnsembleComponent />
+      </div>
+    </div>
+
   </template>
   <template v-else>
-    <div>
-      <h3 class="primary home-subtitle">
-        Veuillez sélectionner le compte acheteur avec lequel vous souhaitez être
-        connecté
-      </h3>
+    <div class="flex flex-col md:flex-row justify-between md:space-x-8">
+      <div class="w-full md:w-1/2 flex flex-col">
+        <div class="lg:w-5/6">
+          <div v-if="showAlert">
+            <AlertSharedComponent />
+          </div>
+          <div class="mb-5">
+            <h1 class="home-subtitle text-gradient">
+              Bonjour {{ userAccounts[0].upplerDatas.master_user.firstname}}
+            </h1>
+            <h3 class="text-gray-500">
+              Veuillez sélectionner le compte acheteur avec lequel vous souhaitez être
+              connecté
+            </h3>
+          </div>
+          <div
+            v-for="(account, id) in userAccounts"
+            :key="id"
+            class="bg-white rounded-md mb-3 p-2 flex flex-col h-[60px]"
+          >
+            <div>
+              <input
+                v-model="accountRadio"
+                name="accountRadio"
+                type="radio"
+                :value="account.id"
+                class="mr-1"
+              />
+              <label class="text-primary font-bold uppercase">
+                {{ account.upplerDatas.name }}
+              </label>
+            </div>
+
+            <label
+              v-if="account.upplerDatas.number"
+              class="text-gray-500 ml-5 font-bold"
+            >
+              SIRET: {{ account.upplerDatas.number }}
+            </label>
+          </div>
+          <div class="flex justify-end">
+            <button
+              class="button button-gradient w-full lg:w-auto"
+              :disabled="isLoading"
+              @click="onAccountClick"
+            >
+              <div v-show="isLoading">
+                <LoaderSharedComponent />
+              </div>
+              <ArrowRightIcon />
+              Valider
+            </button>
+          </div>
+        </div>
     </div>
-    <div v-for="(account, id) in userAccounts" :key="id">
-      <ButtonComponent
-        class="text-cotext mr-2 mb-2 w-auto items-center rounded-md bg-purple-600 px-5 py-2.5 text-sm text-white"
-        :disabled="isLoading"
-        @click="onAccountClick(account)"
-      >
-        {{ account.upplerDatas.name }}
-      </ButtonComponent>
-    </div>
-    <div v-show="isLoading">
-      <LoaderSharedComponent />
+      <div class="sm:w-1/2 flex">
+        <AchetonsEnsembleComponent />
+      </div>
     </div>
   </template>
 </template>
@@ -113,6 +168,8 @@ import MailIcon from '@/vuejs/modules/shared/icon/MailIconComponent.vue'
 import EyeIcon from '@/vuejs/modules/shared/icon/EyeIconComponent.vue'
 import EyeSlashIcon from '@/vuejs/modules/shared/icon/EyeSlashIconComponent.vue'
 import { PHONE_ANIMATION } from '@/vuejs/services/const'
+import AchetonsEnsembleComponent from '@/vuejs/modules/shared/AchetonsEnsembleComponent.vue'
+import { AlertType } from '@/vuejs/types/Alert'
 
 const username = ref<string>('')
 const password = ref<string>('')
@@ -121,6 +178,7 @@ const isLoading = ref<boolean>(false)
 const userStore = useUserStore()
 const alertStore = useAlertStore()
 const showPassword = ref<boolean>(false)
+const accountRadio = ref<string>(null)
 
 const { show: showAlert } = storeToRefs(alertStore)
 
@@ -149,10 +207,19 @@ const loginSubmit = async () => {
 const toggleShowPassword = () => {
   showPassword.value = !showPassword.value
 }
-const onAccountClick = async (account) => {
+const onAccountClick = async () => {
   isLoading.value = true
-  const select = await userStore.selectUserAccount(account.id)
-  select && (document.location.href = '/app/home-page')
-  isLoading.value = false
+  if (accountRadio.value) {
+    const select = await userStore.selectUserAccount(accountRadio.value)
+    select && (document.location.href = '/app/home-page')
+    isLoading.value = false
+  } else {
+    isLoading.value = false
+    alertStore.setShow(
+      'Vous devez sélectionner un compte acheteur pour vous connecter',
+      AlertType.danger,
+    )
+
+  }
 }
 </script>
---
+++ assets/vuejs/modules/shared/AchetonsEnsembleComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2 class="text-primary xl:text-4xl text-2xl font-bold text-left">
+      <span class="bg-gradient-to-r bg-clip-text from-secondary to-gradient-1 text-transparent">
+        Depuis plus de 20 ans,
+      </span><br/>
+      nous achetons mieux ensemble
+    </h2>
+    <div class="flex flex-wrap lg:flex-nowrap lg:mt-8 mt-8 lg:space-x-2">
+      <div class="lg:pr-0 pr-2 w-1/2 lg:w-auto text-left">
+        <LayersIconComponent class="flex h-[44px] w-[44px]" />
+        <h3 class="text-gray-500 text-xl font-bold mt-6">Réalisez 27% d'économies</h3>
+        <div class="text-gray-400">Bénéficiez d'avantages comparables à ceux des grands groupes</div>
+      </div>
+      <div class="pl-2 w-1/2 lg:w-auto text-left">
+        <FolderSearchIconComponent class="flex h-[40px] w-[45px]" />
+        <h3 class="text-gray-500 text-xl font-bold mt-6">Gagnez du temps</h3>
+        <div class="text-gray-400">Restez concentré sur votre coeur de métier</div>
+      </div>
+      <div class="lg:pl-2 lg:pr-0 pr-2 w-1/2 lg:w-auto text-left lg:mt-0 mt-6">
+        <ExpandIconComponent class="flex h-[40px] w-[41px]" />
+        <h3 class="text-gray-500 text-xl font-bold mt-6">Structurez et optimisez vos achats</h3>
+        <div class="text-gray-400">Déployez des accords-cadres déjà négociés, simplement</div>
+      </div>
+      <div class="pl-2 w-1/2 lg:w-auto text-left lg:mt-0 mt-6">
+        <LeafIconComponent class="flex h-[40px] w-[38px]" />
+        <h3 class="text-gray-500 text-xl font-bold mt-6">Commencez votre démarche RSE par vos achats</h3>
+        <div class="text-gray-400">Nous notons nos fournisseurs à l'aide d'un référentiel RSE</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ExpandIconComponent from '@/vuejs/modules/shared/icon/ExpandIconComponent.vue'
+import LeafIconComponent from '@/vuejs/modules/shared/icon/LeafIconComponent.vue'
+import FolderSearchIconComponent from '@/vuejs/modules/shared/icon/FolderSearchIconComponent.vue'
+import LayersIconComponent from '@/vuejs/modules/shared/icon/LayersIconComponent.vue'
+
+</script>
