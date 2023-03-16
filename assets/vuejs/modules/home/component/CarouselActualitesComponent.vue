081764f6ecc2b65934e24a02506e37baf54ffbaa
guy-chavanne <108261066+guy-chavanne@users.noreply.github.com>
2023-03-16 11:34:18 +0100

    MKP-155 Carroussel Actualites

---
+++ assets/vuejs/modules/home/component/CarouselActualitesComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <span v-if="getExpertsContents.length>0">
+    <div class="relative mt-2">
+      <CarouselListSharedComponent
+          :slides-per-view="1"
+          :space-between="20"
+          :breakpoints="{
+              640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+              },
+            }"
+      >
+        <SwiperSlide
+            v-for="expertContent in getExpertsContents"
+            :key="expertContent.id"
+            class="flex h-[303px] items-center justify-center overflow-hidden rounded-lg bg-white xl:h-full"
+        >
+          <img
+              :src="expertContent.slider_img_mobile"
+              alt="Picture"
+              class="flex w-full items-center md:hidden"
+          />
+          <img
+              :src="expertContent.slider_img_desktop"
+              alt="Picture"
+              class="mx-auto hidden items-center md:flex"
+          />
+        </SwiperSlide>
+      </CarouselListSharedComponent>
+    </div>
+  </span>
+</template>
+<script lang="ts" setup>
+
+import CarouselListSharedComponent from '@/vuejs/modules/shared/CarouselListSharedComponent.vue'
+import {SwiperSlide} from 'swiper/vue'
+import {useExpertContentStore} from '@/vuejs/stores/expertContent'
+import {storeToRefs} from 'pinia'
+
+const expertContent = useExpertContentStore()
+const {getExpertsContents} = storeToRefs(expertContent)
+
+</script>
--- assets/vuejs/modules/home/views/HomePage.vue
+++ assets/vuejs/modules/home/views/HomePage.vue
@@ -9,7 +9,7 @@
             à nos 30 000 entreprises adhérentes
           </h3>
           <p
-            class="mt-2.5 text-sm text-gray-400 sm:mt-5 sm:text-base xl:text-lg"
+              class="mt-2.5 text-sm text-gray-400 sm:mt-5 sm:text-base xl:text-lg"
           >
             Cher adhérent, ce nouvel espace dédié vous permet d'acheter
             directement en ligne et de trouver vos accords-cadres, en quelques
@@ -18,52 +18,25 @@
           </p>
         </div>
         <div class="hidden xl:block">
-          <ContactUsButtonComponent />
+          <ContactUsButtonComponent/>
         </div>
       </div>
-      <div class="relative mt-2">
-        <CarouselListSharedComponent
-          :slides-per-view="1"
-          :space-between="20"
-          :breakpoints="{
-            640: {
-              slidesPerView: 1,
-              spaceBetween: 20,
-            },
-          }"
-        >
-          <SwiperSlide
-            v-for="(banniere, key) in bannieres"
-            :key="key"
-            class="flex h-[303px] items-center justify-center overflow-hidden rounded-lg bg-white xl:h-full"
-          >
-            <img
-              :src="banniere.image_mobile"
-              alt="Picture"
-              class="flex w-full items-center md:hidden"
-            />
-            <img
-              :src="banniere.image"
-              alt="Picture"
-              class="mx-auto hidden items-center md:flex"
-            />
-          </SwiperSlide>
-        </CarouselListSharedComponent>
-      </div>
+
+      <CarouselActualitesComponent/>
 
       <!-- Bloc Produits top ventes -->
-      <ProductHomeComponent type="top-vente" title="Top vente" />
+      <ProductHomeComponent type="top-vente" title="Top vente"/>
       <!-- Fin bloc Produits top ventes -->
 
       <!-- Bloc accords cadre -->
-      <AccordCadreComponent />
+      <AccordCadreComponent/>
       <!-- Fin Bloc accords cadre -->
 
       <!-- Bloc sélection de produits -->
       <ProductHomeComponent
-        class="mt-4"
-        type="selection"
-        title="Sélection de produits"
+          class="mt-4"
+          type="selection"
+          title="Sélection de produits"
       />
       <!-- Fin bloc sélection de produits -->
 
@@ -73,23 +46,23 @@
           Plus de 200 partenaires fournisseurs, repartis en 26 catégories, sont
           référencés pour vos achats.
           <a href="#" class="font-normal text-secondary underline"
-            >Découvrir toutes les catégories d'achats</a
+          >Découvrir toutes les catégories d'achats</a
           >
         </p>
       </div>
 
-      <PartnersCarousel class="mt-5" />
-      <OurCategoriesComponent />
+      <PartnersCarousel class="mt-5"/>
+      <OurCategoriesComponent/>
     </div>
 
     <div
-      class="home-bloc-economie text-cotext m-auto mt-16 flex-1 py-4 text-white"
+        class="home-bloc-economie text-cotext m-auto mt-16 flex-1 py-4 text-white"
     >
       <div class="px-5 text-left sm:text-center">
         <h3
-          class="text-[23px] font-bold leading-[27px] sm:text-[35px] sm:leading-[38.11px]"
+            class="text-[23px] font-bold leading-[27px] sm:text-[35px] sm:leading-[38.11px]"
         >
-          Vous faites des économies tout en <br />
+          Vous faites des économies tout en <br/>
           contribuant à votre démarche RSE
         </h3>
         <p class="mt-2 text-sm sm:mx-auto sm:text-base xl:w-[45%] xl:text-lg">
@@ -100,7 +73,7 @@
         </p>
         <p class="mt-10 flex justify-center">
           <a href="#" class="button button-gradient">
-            <ArrowRightIconComponent :stroke-color="'#FFFFFF'" />
+            <ArrowRightIconComponent :stroke-color="'#FFFFFF'"/>
             Toutes nos catégories d'achats
           </a>
         </p>
@@ -109,7 +82,7 @@
 
     <template v-if="getExpertsContents.length">
       <div
-        class="my-6 mx-4 mt-10 max-w-screen-2xl flex-1 rounded-md bg-white pb-4 shadow-md xl:mx-auto"
+          class="my-6 mx-4 mt-10 max-w-screen-2xl flex-1 rounded-md bg-white pb-4 shadow-md xl:mx-auto"
       >
         <div class="flex flex-col pt-1 text-center">
           <h3 class="primary home-subtitle mt-10 flex flex-col pl-8 font-bold">
@@ -119,14 +92,14 @@
               <span class="text-gradient ml-2"> communauté QANTIS </span>
             </p>
           </h3>
-          <ContenusExpertComponent />
+          <ContenusExpertComponent/>
           <div class="flex justify-center">
             <p class="mt-10">
               <RouterLink
-                :to="{ path: '/app/actualites' }"
-                class="button button-gradient"
+                  :to="{ path: '/app/actualites' }"
+                  class="button button-gradient"
               >
-                <ArrowRightIconComponent :stroke-color="'#FFFFFF'" />
+                <ArrowRightIconComponent :stroke-color="'#FFFFFF'"/>
                 Tous les contenus experts
               </RouterLink>
             </p>
@@ -138,27 +111,21 @@
 </template>
 <script lang="ts" setup>
 import BaseTemplate from '@/vuejs/BaseTemplate.vue'
-import CarouselListSharedComponent from '@/vuejs/modules/shared/CarouselListSharedComponent.vue'
-import { getImage, listCategories } from '@/vuejs/services/utils'
-import banniere1 from '@/vuejs/assets/img/demo/banniere-1-desktop.png'
-import banniere2 from '@/vuejs/assets/img/demo/banniere-2-desktop.png'
-import banniereMobile1 from '@/vuejs/assets/img/demo/banniere-1-mobile.png'
-import banniereMobile2 from '@/vuejs/assets/img/demo/banniere-2-mobile.png'
-import { SwiperSlide } from 'swiper/vue'
 import PartnersCarousel from '@/vuejs/modules/shared/PartnersCarouselComponent.vue'
-import { onMounted, ref } from 'vue'
+import {onMounted, ref} from 'vue'
 import ArrowRightIconComponent from '@/vuejs/modules/shared/icon/ArrowRightIconComponent.vue'
 import ContenusExpertComponent from '@/vuejs/modules/home/component/ContenusExpertComponent.vue'
 import DropdownListComponent from '@/vuejs/modules/shared/DropdownListComponent.vue'
 import ProductHomeComponent from '@/vuejs/modules/home/component/ProductHomeComponent.vue'
 import AccordCadreComponent from '@/vuejs/modules/home/component/AccordsCadreComponent.vue'
-import { useExpertContentStore } from '@/vuejs/stores/expertContent'
-import { storeToRefs } from 'pinia'
+import {useExpertContentStore} from '@/vuejs/stores/expertContent'
+import {storeToRefs} from 'pinia'
 import ContactUsButtonComponent from '@/vuejs/modules/shared/ContactUsButtonComponent.vue'
-import OurCategoriesComponent from '@/vuejs/modules/home/component/OurCategoriesComponent.vue';
+import OurCategoriesComponent from '@/vuejs/modules/home/component/OurCategoriesComponent.vue'
+import CarouselActualitesComponent from '@/vuejs/modules/home/component/CarouselActualitesComponent.vue'
 
 const expertContentStore = useExpertContentStore()
-const { getExpertsContents } = storeToRefs(expertContentStore)
+const {getExpertsContents} = storeToRefs(expertContentStore)
 
 const expertsContentsLoaded = ref<boolean>(false)
 
@@ -167,10 +134,6 @@ onMounted(async () => {
   expertsContentsLoaded.value = true
 })
 
-const bannieres = ref([
-  { image: getImage(banniere1), image_mobile: getImage(banniereMobile1) },
-  { image: getImage(banniere2), image_mobile: getImage(banniereMobile2) },
-])
 </script>
 
 <style lang="scss">
