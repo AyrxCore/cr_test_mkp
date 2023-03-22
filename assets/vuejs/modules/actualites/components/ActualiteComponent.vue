45272e9e95c1552ab6f71f866959f8caedf2f069
Adrien Fourel <ad.fourel@gmail.com>
2023-03-22 12:01:46 +0100

    MISC - use date-fns instead of moment

--- assets/vuejs/modules/actualites/components/ActualiteComponent.vue
+++ assets/vuejs/modules/actualites/components/ActualiteComponent.vue
@@ -1,59 +1,53 @@
 <template>
-  <div
-      class="mx-auto flex h-[524px] flex-col items-center rounded-md bg-white px-6 py-2"
-  >
+  <div class="mx-auto flex h-[524px] flex-col rounded-md bg-white px-6 py-4">
     <div class="mx-auto items-center">
       <RouterLink
-        :to="{ name: PageList.ACTUALITE, params: {slug: contenu.slug} }"
+        :to="{ name: PageList.ACTUALITE, params: { slug: contenu.slug } }"
       >
         <img
-            :src="contenu.page_actus_img_desktop"
-            alt="Image produit"
-            class="mx-auto h-[205px!important] w-[334px!important]"
+          :src="contenu.page_actus_img_desktop"
+          :alt="`Image ${contenu.articleTitle}`"
+          class="mx-auto h-[205px!important] w-[334px!important]"
         />
       </RouterLink>
     </div>
-    <div class="mt-5 w-full justify-start">
+    <div class="mt-5">
       <p
-          class="mb-3 w-max rounded-md px-2 py-1 text-white"
-          :style="{'background': contenu.categoryColor}"
+        class="mb-3 w-max rounded-md px-2 py-1 text-white"
+        :style="{ background: contenu.categoryColor }"
       >
         {{ contenu.categoryName }}
       </p>
       <span class="text-gray-500">
-        {{ moment(String(contenu.date)).format('DD/MM/YYYY') }}
-
+        {{ formattedDate }}
       </span>
     </div>
-    <h3 class="mt-6 text-left text-[25px] font-bold text-primary ">
+    <h3 class="mt-2 text-[25px] font-bold text-primary">
       <RouterLink
-        :to="{ name: PageList.ACTUALITE, params: {slug: contenu.slug} }"
-        class="bottom-0 items-center text-primary truncate-custom truncate-custom-2"
+        :to="{ name: PageList.ACTUALITE, params: { slug: contenu.slug } }"
+        class="truncate-custom truncate-custom-2 text-primary"
       >
         {{ contenu.articleTitle }}
       </RouterLink>
     </h3>
-    <p class="mt-1 text-left text-lg text-gray-400 truncate-custom truncate-custom-3">
+    <p class="truncate-custom truncate-custom-3 mt-1 text-lg text-gray-400">
       {{ contenu.articleTeaser }}
     </p>
-    <div class="bottom-0 flex w-full items-end">
-      <RouterLink
-        :to="{ name: PageList.ACTUALITE, params: {slug: contenu.slug} }"
-        class="bottom-0 items-center text-sm font-medium text-primary underline"
-      >
-        Lire l'article
-      </RouterLink>
-    </div>
+    <RouterLink
+      :to="{ name: PageList.ACTUALITE, params: { slug: contenu.slug } }"
+      class="text-sm font-medium text-primary underline"
+    >
+      Lire l'article
+    </RouterLink>
   </div>
 </template>
-<script lang="ts" setup>
-import {useRouter} from 'vue-router'
-import {PageList} from '@/vuejs/router'
-import {ExpertContent} from '@/vuejs/types/ExpertContent'
-import {PropType} from 'vue'
-import moment from 'moment'
 
-const router = useRouter()
+<script lang="ts" setup>
+import { format } from 'date-fns'
+import { computed, PropType } from 'vue'
+
+import { PageList } from '@/vuejs/router'
+import { ExpertContent } from '@/vuejs/types/ExpertContent'
 
 const props = defineProps({
   contenu: {
@@ -62,10 +56,9 @@ const props = defineProps({
   },
 })
 
-const goToArticle = (slug) => {
-  router.push({name: PageList.ACTUALITE, params: {slug: slug}})
-}
-
+const formattedDate = computed((): string => {
+  return format(new Date(props.contenu.date), 'dd/MM/yyyy')
+})
 </script>
 
 <style scoped></style>
--- assets/vuejs/modules/actualites/views/Actualite.vue
+++ assets/vuejs/modules/actualites/views/Actualite.vue
@@ -4,42 +4,50 @@
       v-if="currentExpertContent"
       class="xs:w-[100%] m-auto my-4 max-w-screen-2xl flex-1 px-5 sm:px-8"
     >
-      <breadcrumb-shared-component
-          :list-url="listUrl"
-          :current-page="'Actualité'"
+      <BreadcrumbSharedComponent
+        :list-url="listUrl"
+        :current-page="'Actualité'"
       />
       <div class="w-[100%] max-w-screen-2xl">
-        <ContactUsButtonComponent/>
+        <ContactUsButtonComponent />
       </div>
       <div
-          class="m-auto my-2 flex w-[100%] max-w-screen-2xl flex-col lg:grid lg:grid-cols-2 lg:gap-4"
+        class="m-auto my-2 flex w-[100%] max-w-screen-2xl flex-col lg:grid lg:grid-cols-2 lg:gap-4"
       >
         <!-- Bloc text actualité -->
         <div>
-          <h3 class="primary text-title-35 mb-2">
+          <h3 class="primary mb-2 text-title-35">
             {{ currentExpertContent.articleTitle }}
           </h3>
           <span
-              class="mr-2 w-max rounded-md px-2 py-1 text-white text-white"
-              :style="{'background': currentExpertContent.categoryColor}"
-          >{{ currentExpertContent.categoryName }}</span
+            class="mr-2 w-max rounded-md px-2 py-1 text-white"
+            :style="{ background: currentExpertContent.categoryColor }"
           >
-          <span class="text-gray-500">{{ moment(String(currentExpertContent.date)).format('DD/MM/YYYY') }}</span>
+            {{ currentExpertContent.categoryName }}
+          </span>
+          <span class="text-gray-500">
+            {{ formattedDate }}
+          </span>
           <div class="mt-5 h-[auto] rounded-lg">
             <p
               class="whitespace-pre-line text-gray-500"
               v-html="currentExpertContent.articleContent"
             />
           </div>
-          <div v-if="currentExpertContent.ctaTxt !== '' && currentExpertContent.ctaLink !== ''">
+          <div
+            v-if="
+              currentExpertContent.ctaTxt !== '' &&
+              currentExpertContent.ctaLink !== ''
+            "
+          >
             <a
-                :href="currentExpertContent.ctaLink"
-                target="_blank"
-                class="default-button-gradient mt-4 inline-flex justify-center px-3.5 py-3 text-center font-bold text-white"
+              :href="currentExpertContent.ctaLink"
+              target="_blank"
+              class="default-button-gradient mt-4 inline-flex justify-center px-3.5 py-3 text-center font-bold text-white"
             >
               <ArrowRigntIconComponent
-                  class="mt-1 mr-2 w-4 items-center"
-                  :stroke-color="'#FFFFFF'"
+                class="mt-1 mr-2 w-4 items-center"
+                :stroke-color="'#FFFFFF'"
               />
               {{ currentExpertContent.ctaTxt }}
             </a>
@@ -48,18 +56,18 @@
         <!-- Fin Bloc text actualité -->
 
         <!-- Bloc image -->
-        <div class="mt-[7rem] h-[421px] rounded-lg bg-white hidden md:flex">
+        <div class="mt-[7rem] hidden h-[421px] rounded-lg bg-white md:flex">
           <img
-              :src="currentExpertContent.article_img_desktop"
-              alt="Picture"
-              class="m-auto h-[inherit] items-center"
+            :src="currentExpertContent.article_img_desktop"
+            alt="Picture"
+            class="m-auto h-[inherit] items-center"
           />
         </div>
         <div class="mt-[7rem] h-[421px] rounded-lg bg-white md:hidden">
           <img
-              :src="currentExpertContent.article_img_mobile"
-              alt="Picture"
-              class="m-auto h-[inherit] items-center"
+            :src="currentExpertContent.article_img_mobile"
+            alt="Picture"
+            class="m-auto h-[inherit] items-center"
           />
         </div>
         <!-- Fin Bloc image -->
@@ -68,14 +76,11 @@
       <!-- Bloc articles recommandés -->
       <div class="mt-10 justify-center">
         <h3 class="primary home-subtitle mb-5">Articles recommandés</h3>
-        <ContenusExpertComponent :contenus="contenusExpert"/>
+        <ContenusExpertComponent :contenus="contenusExpert" />
       </div>
       <!-- Fin Bloc articles recommandés -->
     </div>
-    <div
-      v-else
-      class="mt-5 flex h-20 w-full items-center justify-center"
-    >
+    <div v-else class="mt-5 flex h-20 w-full items-center justify-center">
       <LoaderSharedComponent
         class="text-secondary"
         classes="loader-xl loader"
@@ -83,19 +88,22 @@
     </div>
   </BaseTemplate>
 </template>
+
 <script lang="ts" setup>
-import BaseTemplate from '@/vuejs/BaseTemplate.vue'
-import {computed, ref, watch} from 'vue'
-import ContactUsButtonComponent from '@/vuejs/modules/shared/ContactUsButtonComponent.vue'
-import BreadcrumbSharedComponent from '@/vuejs/modules/shared/BreadcrumbSharedComponent.vue'
+import { format } from 'date-fns'
+import { computed, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+
 import ArrowRigntIconComponent from '@/vuejs/modules/shared/icon/ArrowRightIconComponent.vue'
-import {contenusExpert} from '@/vuejs/modules/actualites'
+import BaseTemplate from '@/vuejs/BaseTemplate.vue'
+import BreadcrumbSharedComponent from '@/vuejs/modules/shared/BreadcrumbSharedComponent.vue'
+import ContactUsButtonComponent from '@/vuejs/modules/shared/ContactUsButtonComponent.vue'
 import ContenusExpertComponent from '@/vuejs/modules/home/component/ContenusExpertComponent.vue'
-import {useExpertContentStore} from '@/vuejs/stores/expertContent'
-import {useRoute} from 'vue-router'
-import {ExpertContent} from '@/vuejs/types/ExpertContent'
-import moment from 'moment'
-import LoaderSharedComponent from '@/vuejs/modules/shared/LoaderSharedComponent.vue';
+import LoaderSharedComponent from '@/vuejs/modules/shared/LoaderSharedComponent.vue'
+
+import { contenusExpert } from '@/vuejs/modules/actualites'
+import { useExpertContentStore } from '@/vuejs/stores/expertContent'
+import { ExpertContent } from '@/vuejs/types/ExpertContent'
 
 const route = useRoute()
 const expertContentStore = useExpertContentStore()
@@ -108,16 +116,20 @@ const listUrl = ref([
   },
 ])
 
-watch(
-    () => route.params.slug as string,
-    async (slug: string) => {
-      if (slug) {
-        console.log(moment(String('2023-02-15')).format('MM/DD/YYYY'))
-        currentExpertContent.value = await expertContentStore.initActualitePage(slug)
-      }
+const formattedDate = computed((): string => {
+  return format(new Date(currentExpertContent.value.date), 'dd/MM/yyyy')
+})
 
-    },
-    {immediate: true},
+watch(
+  () => route.params.slug as string,
+  async (slug: string) => {
+    if (slug) {
+      currentExpertContent.value = await expertContentStore.initActualitePage(
+        slug,
+      )
+    }
+  },
+  { immediate: true },
 )
 </script>
 
