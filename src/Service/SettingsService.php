7845db3c517a2b6cd9d89e15a06c04f36704a68b
Ayrx <a.roux@qantis.co>
2023-10-13 15:38:46 +0200

    QAN-720/fix:add param type and return type in SettingsService functions

--- src/Service/SettingsService.php
+++ src/Service/SettingsService.php
@@ -2,23 +2,24 @@
 
 namespace App\Service;
 
+use App\Entity\Setting;
 use App\Repository\SettingRepository;
 
 class SettingsService
 {
     public function __construct(private SettingRepository $settingRepository){}
 
-    public function getSetting($settingName)
+    public function getSetting(string $settingName): ?Setting
     {
         return $this->settingRepository->findOneBy(['name' => $settingName]);
     }
 
-    public function useExternalScriptsTags()
+    public function useExternalScriptsTags(): bool|string|null
     {
         return !$this->getSetting('external_scripts_tags') ? false : $this->getSetting('external_scripts_tags')->getValue();
     }
 
-    public function isMaintenanceMode()
+    public function isMaintenanceMode(): bool|string|null
     {
         return !$this->getSetting('maintenance_mode') ? false : $this->getSetting('maintenance_mode')->getValue();
     }
