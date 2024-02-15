7f5e26ecd4db92ff7122d7ef9ed854a36c99d3f8
dapaul-qantis <113889695+dapaul-qantis@users.noreply.github.com>
2024-02-15 22:58:52 +0100

    fix(QAN-922) Send email to the channel email contact (#307)
    * fix(QAN-922) Send email to the channel email contact
    
    * Creat a service to send email after FAT subscription

--- src/Controller/Api/Buyer/ProductApiController.php
+++ src/Controller/Api/Buyer/ProductApiController.php
@@ -5,21 +5,15 @@ declare(strict_types=1);
 namespace App\Controller\Api\Buyer;
 
 use App\Context\ChannelContext;
-use App\Dto\AccountAccordCadre;
-use App\Entity\AccordStatut;
-use App\Entity\Account;
-use App\Entity\LogAccordStatutRequest;
-use App\Service\MailerProvider;
+use App\Service\AccordCadreSubscriptionService;
 use App\Service\UpplerProductService;
-use Doctrine\ORM\EntityManagerInterface;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Uid\Uuid;
-use Twig\Environment;
 
 class ProductApiController extends AbstractController
 {
@@ -28,9 +22,8 @@ class ProductApiController extends AbstractController
 
     public function __construct(
         private RequestStack $requestStack,
-        private EntityManagerInterface $em,
         private UpplerProductService $upplerProductService,
-        private Environment $twig
+        private AccordCadreSubscriptionService $accordCadreSubscriptionService,
     ) {
     }
 
@@ -42,107 +35,29 @@ class ProductApiController extends AbstractController
         return new JsonResponse($variant);
     }
 
+    /**
+     * @throws \Exception
+     */
     #[Route('/api/accord-cadre-subscription', name: 'accord_cadre_subscription', methods: ['POST'])]
     public function subscription(
         Request $request,
-        MailerProvider $mailerProvider,
-        LoggerInterface $logger,
         ChannelContext $channelContext
     ): JsonResponse {
         $session = $this->requestStack->getSession();
 
-        $params = \json_decode($request->getContent(), true);
+        if (
+            !$session->has('account') || empty($session->get('account'))
+            || !$session->has('access_token') || empty($session->get('access_token'))
+        ) {
+            throw new AccessDeniedException('Access denied to make subscription. Please login.');
+        }
         $accountId = (string) $session->get('account')->getId();
-        $account = $this->em->getRepository(Account::class)->find($accountId);
 
-        $accordStatut = $this->em->getRepository(AccordStatut::class)->findOneBy([
-            'adherent' => $account->getAdherent()->getId(),
-            'accordId' => $params['accordId'],
-        ]);
-
-        $error = false;
-        try {
-            $sugarLink = $this->getParameter('SUBSCRIPTION_MAIL_SUGAR_LINK');
-            $from = $this->getParameter('SUBSCRIPTION_MAIL_FROM');
-            $to = $channelContext->getChannel()?->getChannelParameter()?->getEmail();
-
-            $mailerProvider->send(
-                $from,
-                $to,
-                'MARKETPLACE - Bénéficier des conditions pour la FAT '.$params['accordName'],
-                $this->twig->render('mails/request.accord.subscription.html.twig', [
-                    'fat' => $params['accordName'],
-                    'email' => $account->getUser()->getemail(),
-                    'nom' => $account->getUser()->getFirstName().' '.$account->getUser()->getLastName(),
-                    'societe' => $account->getAdherent()->getName(),
-                    'sugarLink' => $sugarLink.$account->getAdherent()->getId(),
-                ])
-            );
-
-            // STELLANTIS
-            $parameters = $this->getParameter('STELLANTIS_MAILING');
-            if (\in_array($params['accordId'], $parameters['ACCORDS_IDS'], true)) {
-                // send adherent service mail
-                $mailerProvider->send(
-                    $parameters['ADHERENT_MAIL']['FROM'],
-                    \explode(';', $parameters['ADHERENT_MAIL']['TO']),
-                    'Marketplace - '.$account->getAdherent()->getSiret().' - Demande de rattachement au contrat QANTIS/STELLANTIS',
-                    $this->twig->render('mails/stellantis/to_adherent_service.html.twig', [
-                        'account' => $account,
-                        'horodatage' => new \DateTime('now'),
-                    ]),
-                );
-
-                // send Stellantis mail
-                $mailerProvider->send(
-                    $parameters['STELLANTIS_MAIL']['FROM'],
-                    \explode(';', $parameters['STELLANTIS_MAIL']['TO']),
-                    $account->getAdherent()->getSiret().' - Demande de rattachement au contrat STELLANTIS',
-                    $this->twig->render('mails/stellantis/to_stellantis.html.twig', [
-                        'account' => $account,
-                        'horodatage' => new \DateTime('now'),
-                    ]),
-                    ['cc' => \explode(',', $parameters['STELLANTIS_MAIL']['CC'])],
-                );
-            }
-        } catch (\Exception $exception) {
-            $error = true;
-            $logger->critical(
-                "Erreur d'envoi de demande de subscription "
-                .$account->getUser()->getemail().' '.$account->getAdherent()->getName().' : '.
-                $exception->getMessage()
-            );
-        }
-        if ($error) {
-            $accordStatut = new AccordStatut();
-            $accordStatut->setAdherent($account->getAdherent());
-            $accordStatut->setAccordId(new Uuid($params['accordId']));
-            $accordStatut->setStatus(AccountAccordCadre::PROCESS_STATUS_NOT_ACTIVATED);
-            $accordStatut->setAccordStatutRequestAt(new \DateTime('now'));
-        } elseif ($accordStatut) {
-            if ($accordStatut->getStatus() === AccountAccordCadre::PROCESS_STATUS_NOT_ACTIVATED) {
-                $accordStatut->setStatus(AccountAccordCadre::PROCESS_STATUS_PENDING);
-
-                $this->em->persist($accordStatut);
-                $this->em->flush();
-            }
-        } else {
-            $accordStatut = new AccordStatut();
-            $accordStatut->setAdherent($account->getAdherent());
-            $accordStatut->setAccordId(new Uuid($params['accordId']));
-            $accordStatut->setStatus(AccountAccordCadre::PROCESS_STATUS_PENDING);
-            $accordStatut->setAccordStatutRequestAt(new \DateTime('now'));
-            $this->em->persist($accordStatut);
-
-            $log = new LogAccordStatutRequest();
-            $log->setAccordId(new Uuid($params['accordId']));
-            $log->setAccount($account);
-            $log->setCreatedAt(new \DateTimeImmutable('now'));
-            $this->em->persist($log);
-
-            $this->em->flush();
+        $params = \json_decode($request->getContent(), true);
+        if (!isset($params['accordId'], $params['accordName'])) {
+            throw new BadRequestHttpException('Missing required parameters.');
         }
 
-        return new JsonResponse($accordStatut->getStatus());
+        return new JsonResponse($this->accordCadreSubscriptionService->subscription($params, $accountId, $channelContext));
     }
 }
