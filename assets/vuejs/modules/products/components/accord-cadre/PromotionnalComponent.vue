a86a4dcafb1c1c45bd1ebe477366fac23232d69e
Ayrx <45879664+AyrxCore@users.noreply.github.com>
2024-02-20 10:10:54 +0100

    QAN-952/add width for img and text to avoid img shrinking by text (#312)
    Co-authored-by: Ayrx <a.roux@qantis.co>

--- assets/vuejs/modules/products/components/accord-cadre/PromotionnalComponent.vue
+++ assets/vuejs/modules/products/components/accord-cadre/PromotionnalComponent.vue
@@ -8,10 +8,10 @@
     class="flex flex-col items-center rounded-md bg-secondary p-6 lg:flex-row"
     :class="'text-' + betterTextColor('secondary')"
   >
-    <div class="flex max-w-screen-md items-center justify-center">
+    <div class="flex max-w-screen-md items-center justify-center lg:w-5/12">
       <img :src="properties.fat_promo_img" alt="Image promo" />
     </div>
-    <div class="my-8 flex flex-col justify-center lg:pl-6">
+    <div class="my-8 flex flex-col justify-center lg:w-7/12 lg:pl-6">
       <h3 class="mb-8 text-3xl font-bold">{{ properties.fat_promo_titre }}</h3>
       <div class="mb-8 text-lg" v-html="properties.fat_promo_txt" />
       <ButtonComponent
