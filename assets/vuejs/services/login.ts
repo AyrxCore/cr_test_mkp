d2ef04fcf55797ff3275754252e76d19fd4b8ed8
dapaul-qantis <113889695+dapaul-qantis@users.noreply.github.com>
2024-03-26 10:42:11 +0100

    fix(QAN-1151) Change error message when user failed to connect or when user account is not activated yet (#328)

--- assets/vuejs/services/login.ts
+++ assets/vuejs/services/login.ts
@@ -5,14 +5,15 @@ export function getErrorMessage(errorCode: string): string {
   switch (errorCode) {
     case LoginResponse.UserDisabled:
       message =
-        "Votre compte n'est pas actif, vous devez utiliser le lien de première connexion pour initialiser votre accès"
+        "Votre compte n'est pas actif. Pour l'activer, cliquez sur le bouton “activer mon compte” et suivez les instructions."
       break
     case LoginResponse.UserEmptyAccount:
       message =
         'Aucun compte ne correspond à cet email sur notre plateforme, veuillez contacter le service client'
       break
     case LoginResponse.InvalidCredentials:
-      message = 'Identifiants incorrects'
+      message =
+        'Votre adresse email et/ou votre mot de passe semblent incorrects - veuillez réessayer'
       break
     default:
       message = 'Une erreur est survenue'
