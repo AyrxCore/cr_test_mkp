2d7f5d39271d4a4a97f59811dc43ec6eeee6ade0
jayhem10 <noble.jka@gmail.com>
2024-07-18 10:51:13 +0200

    QAN-1464: fix categories display

--- assets/vuejs/modules/products/components/filters/FilterCategoryComponent.vue
+++ assets/vuejs/modules/products/components/filters/FilterCategoryComponent.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="my-2 flex items-center text-left text-sm">
-    <input
-      v-model="catRadio"
-      :checked="category.checked"
-      :value="category.id"
-      :id="`categoryRadio-${category.id}`"
-      name="catRadio"
-      type="radio"
-      class="mr-3 cursor-pointer"
-      @change="handleCategorySelection(category)"
-    />
-    <label :for="`categoryRadio-${category.id}`" class="cursor-pointer">
-      {{ category.name }} ({{ category.productCount }})
-    </label>
-    <Chevron2RightIconComponent
-      v-if="category.children?.length > 0"
-      :class="{
-        'ml-2 h-3 w-6 font-bold': !category.parentId,
-        'mr-2': category.parentId !== null,
-        'mt-4 rotate-90 ease-in-out': showChildren,
-      }"
-      @click="toggleChildren"
-    />
-    <div v-if="showChildren" class="ml-5">
+  <div class="my-2 text-left text-sm">
+    <div class="flex items-center">
+      <input
+        v-model="catRadio"
+        :checked="category.checked"
+        :value="category.id"
+        :id="`categoryRadio-${category.id}`"
+        name="catRadio"
+        type="radio"
+        class="mr-3 cursor-pointer"
+        @change="handleCategorySelection(category)"
+      />
+      <label :for="`categoryRadio-${category.id}`" class="cursor-pointer">
+        {{ category.name }} ({{ category.productCount }})
+      </label>
+      <Chevron2RightIconComponent
+        v-if="category.children?.length > 0"
+        :class="{
+          'ml-2 h-3 w-6 font-bold': !category.parentId,
+          'mr-2': category.parentId !== null,
+          'rotate-90 ease-in-out': showChildren,
+        }"
+        @click="toggleChildren"
+      />
+    </div>
+    <div v-if="showChildren" class="ml-4 mt-2">
       <FilterCategoryComponent
         v-for="cat in category.children"
         :key="cat.id"
@@ -32,6 +34,7 @@
     </div>
   </div>
 </template>
+
 <script lang="ts" setup>
 import { PropType, ref } from 'vue'
 import Chevron2RightIconComponent from '@/vuejs/modules/shared/icon/Chevron2RightIconComponent.vue'
